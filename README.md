8dd6e85ff5c974f91af65a999ee1ff4f4e0340ee
Monstrous <49096535+MonstrousSoftware@users.noreply.github.com>
2026-08-27 00:07:46 +0200

    mouse-look, head bob, jumping

--- README.md
+++ README.md
@@ -5,6 +5,12 @@ This project is part of the Tutorial on creating a 3D game with LibGDX:
 https://monstroussoftware.github.io/2023/11/01/Tutorial-3D-step1.html
 
 
+To move use WASD keys.  Press SHIFT to go faster.
+Use Q and E to turn (or mouse).
+Press SPACE to jump.
+Press ESC to quit.
+
+
 ## Platforms
 
 - `core`: Main module with the application logic shared by all platforms.
--- core/src/main/java/com/monstrous/tut3d/Settings.java
+++ core/src/main/java/com/monstrous/tut3d/Settings.java
@@ -7,4 +7,11 @@ public class Settings {
     static public float walkSpeed = 5f;    // m/s
     static public float runFactor = 3f;    // m/s
     static public float turnSpeed = 120f;   // degrees/s
+
+    static public boolean invertLook = false;
+    static public boolean freeLook = true;
+    static public float headBobDuration = 0.6f; // s
+    static public float headBobHeight = 0.04f;  // m
+
+    static public float gravity = -9.8f; // meters / s^2
 }
