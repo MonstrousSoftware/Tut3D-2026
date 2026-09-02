f02577f0758ced3dd808cae91098b2fae1ddedc4
Monstrous <49096535+MonstrousSoftware@users.noreply.github.com>
2026-09-02 19:32:40 +0200

    coin collision shape & mass

--- core/src/main/java/com/monstrous/tut3d/Populator.java
+++ core/src/main/java/com/monstrous/tut3d/Populator.java
@@ -18,9 +18,9 @@ public class Populator {
         world.spawnObject(GameObjectType.TYPE_DYNAMIC, "ball", null, CollisionShapeType.SPHERE, true, new Vector3(-2,6,-2), Settings.ballMass);
         world.spawnObject(GameObjectType.TYPE_STATIC,"arch",  null, CollisionShapeType.MESH, false, Vector3.Zero, 1f);
         world.spawnObject(GameObjectType.TYPE_STATIC,"stairs",   "stairsProxy", CollisionShapeType.MESH, false, Vector3.Zero, 1f);
-        world.spawnObject(GameObjectType.TYPE_PICKUP_COIN, "coin",  null, CollisionShapeType.SPHERE, true, new Vector3(-5, 1, 0), 1);
-        world.spawnObject(GameObjectType.TYPE_PICKUP_COIN, "coin",  null,  CollisionShapeType.SPHERE, true, new Vector3(5, 1, 15), 1);
-        world.spawnObject(GameObjectType.TYPE_PICKUP_COIN, "coin",  null, CollisionShapeType.SPHERE, true, new Vector3(-12, 1, 13), 1);
+        world.spawnObject(GameObjectType.TYPE_PICKUP_COIN, "coin",  null, CollisionShapeType.BOX, true, new Vector3(-5, 1, 0), 0.1f);
+        world.spawnObject(GameObjectType.TYPE_PICKUP_COIN, "coin",  null,  CollisionShapeType.BOX, true, new Vector3(5, 1, 15), 0.1f);
+        world.spawnObject(GameObjectType.TYPE_PICKUP_COIN, "coin",  null, CollisionShapeType.BOX, true, new Vector3(-12, 1, 13), 0.1f);
         world.spawnObject(GameObjectType.TYPE_PICKUP_HEALTH, "healthpack",null, CollisionShapeType.BOX, true, new Vector3(26, 0.1f, -26), 1);
         world.spawnObject(GameObjectType.TYPE_PICKUP_HEALTH, "healthpack",  null, CollisionShapeType.BOX, true, new Vector3(-26, 0.1f, 26), 1);
         /// add enemies
